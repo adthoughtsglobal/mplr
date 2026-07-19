5a1dec3e0c3af725029d4a4fab7726c409c922b0
adthoughtsglobal <adthoughtsglobal@gmail.com>
2026-07-19 20:56:09 +0530

    setup pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     plugins: [solid()],
     base: "./",
     build: {
-        base: command === "build" ? "/mplr/" : "/",
+        plugins: [solid()],
+        base: "/mplr/",
     }
 });
